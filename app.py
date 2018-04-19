169fc943db0a56adf3c06572111b3b8a37d1a7e7
Navneet suman <navneet35371@gmail.com>
2018-04-19 16:12:27 +0530

    Fixes the port issue

--- app.py
+++ app.py
@@ -26,9 +26,12 @@ class StockSearchService(object):
         return get_stock_by_name(q)
 
 if __name__ == "__main__":
-    cherrypy.config.update({'server.socket_port': int(os.environ.get('PORT', 5000))})
 
     conf = {
+        'global': {
+            'server.socket_host': '0.0.0.0',
+            'server.socket_port': int(os.environ.get('PORT', 5000))
+        },
         '/': {
             'tools.sessions.on': True,
             'tools.staticdir.root': os.path.abspath(os.getcwd())
